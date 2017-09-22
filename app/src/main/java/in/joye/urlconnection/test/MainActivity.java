a75dbcfc5e50b506b7bf8eb6924575bb498a68b3
Joye <zhangzhichao@qfpay.com>
2017-09-22 09:42:16 +0800

    Compile as single library

--- app/src/main/java/in/joye/urlconnection/test/MainActivity.java
+++ app/src/main/java/in/joye/urlconnection/test/MainActivity.java
@@ -1,7 +1,9 @@
-package in.joye.urlconnection;
+package in.joye.urlconnection.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import in.joye.urlconnection.R;
 
 public class MainActivity extends AppCompatActivity {
 
